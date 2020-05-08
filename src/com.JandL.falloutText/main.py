93a59271d7509c14cac2d092c7c8a9d52dfa4ef8
burkejasonj <burke.stevej@outlook.com>
2020-05-07 21:24:33 -0700

    add default code for game engine and logic engine

--- src/com.JandL.falloutText/main.py
+++ src/com.JandL.falloutText/main.py
@@ -1,6 +1,7 @@
 import json
 import io
 import os
+import gameEngine as engine
 
 
 def main():
@@ -10,34 +11,43 @@ def main():
 
     # Init screen
 
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
 
     # Ask for ruleset name and version
 
-    rulesetManifest = io.open("rulesets/manifest.json", 'r')
+    rulesetManifest = io.open("rulesets/manifest.json", "r")
     rulesetManifest = json.load(rulesetManifest)
     print("Please select one of the following:")
-    for slot in rulesetManifest['availableGames']:
-        print(rulesetManifest['availableGames'][slot]['callName'])
+    for slot in rulesetManifest["availableGames"]:
+        print(rulesetManifest["availableGames"][slot]["callName"])
     slotID = input("? ")
 
     # Get ruleset object from json
 
-    for slot in rulesetManifest['availableGames']:
-        if slotID == rulesetManifest['availableGames'][slot]['callName']:
-            # TODO: Add try/except for manifest.json
-            rules = io.open(rulesetManifest['availableGames'][slot]['manifestLocation'])
+    for slot in rulesetManifest["availableGames"]:
+        if slotID == rulesetManifest["availableGames"][slot]["callName"]:
+            # TODO: Add try/except for ruleset.json
+            rules = io.open(rulesetManifest["availableGames"][slot]["manifestLocation"])
             rules = json.load(rules)
             break
         else:
             continue
 
-    print(rules['gameInfo']['title']+' Version '+str(rules['gameInfo']['version'])+' Branch '+rules['gameInfo']['branch']+'\nRules by '+rules['gameInfo']['author'])
+    print(
+        rules["gameInfo"]["title"]
+        + " Version "
+        + str(rules["gameInfo"]["version"])
+        + " Branch "
+        + rules["gameInfo"]["branch"]
+        + "\nRules by "
+        + rules["gameInfo"]["author"]
+    )
 
     # TODO: run game
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
